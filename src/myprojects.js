3e9e3a7d219c01ceeb2c58a68ef8a645e956ead2
tuanhphung <52213081+tuanhphung@users.noreply.github.com>
2021-03-12 18:16:23 +0000

    added new projects

--- src/myprojects.js
+++ src/myprojects.js
@@ -1,4 +1,5 @@
 import portfolioimg from "./assets/projectimgs/portfolioSS.jpg";
+import budgetappimg from "./assets/projectimgs/budgetappSS.jpg";
 
 const myprojects = [
   {
@@ -24,7 +25,7 @@ const myprojects = [
       <div>
         <p>Technologies Used :</p>
         <ul>
-          <li>Javascript / React</li>
+          <li>JavaScript / React</li>
           <li>CSS</li>
           <li>Framer-Motion</li>
           <li>React-Intersection-Observer </li>
@@ -34,6 +35,41 @@ const myprojects = [
     githublink: "https://github.com/tuanhphung/myportfolio",
     demolink: "https://tuanhphung.netlify.app/",
     img: portfolioimg,
+  },
+  {
+    title: "Budget App",
+    description: (
+      <div>
+        <p>
+          A simple app used to track your budget.
+          <br />
+          You can enter the amount and description of income and expenses. The budget
+          amount is dynamic so that it calculates and updates as its expense and incomes
+          is added or removed.
+        </p>
+        <p>
+          This was built when learning vanilla JavaScript JavaScript around the Model View
+          Controller model (MVC), to seperate each sections of the code to handle
+          logic(Model), visually display data(View) and handle event listeners(Controller)
+        </p>
+        <p>
+          I aim to rebuild this project using the React in the future, as I have now
+          learnt the framework
+        </p>
+      </div>
+    ),
+    technologies: (
+      <div>
+        <ul>
+          <li>JavaScript</li>
+          <li>CSS</li>
+          <li>HTML</li>
+        </ul>
+      </div>
+    ),
+    githublink: "https://github.com/tuanhphung/BudgetyApp",
+    demolink: " ",
+    img: budgetappimg,
   },
 ];
 
